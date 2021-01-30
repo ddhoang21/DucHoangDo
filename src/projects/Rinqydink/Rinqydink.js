a0b76b2ce104b840de9f034bee5ae937081303f6
DDH <ddhoang1993@gmail.com>
2021-01-29 20:50:27 -0800

    added description and link for rinqydink project

--- src/projects/Rinqydink/Rinqydink.js
+++ src/projects/Rinqydink/Rinqydink.js
@@ -26,10 +26,16 @@ const Rinqydink = () => {
                             <Container className='text-center'>
                                 <h1 className='rinqydink-project-title mt-4'>Rinqydink Arcade</h1>
                                 <p className='rinqydink-project-subtitle mt-4'>An online gaming web app letting users compete and interact with each other</p>
+                                <Button className='back-btn shadow-none' variant='link' href='https://rinqydinky.herokuapp.com/' target='_blank'><p className='back-btn-text'><strong>Visit website</strong></p></Button>
                             </Container>
                         </Container>
                 </Jumbotron>
                 <Container>
+                    <Jumbotron className='gallery-jumbotron'>
+                        <p className='vkbe-text'>This project is significant to me because it ignited my passion for front end development. I realized that it was a career I wanted to pursue.</p>
+                        <p className='vkbe-text'>I was responsible for the website's interface and understood how critical the users' experience is when designing an application.</p>
+                        <p className='vkbe-text'>I was also in charge of the users' authentication which is where I struggled the most but also acknowledged the importance of security and protection of users' credentials.</p>
+                    </Jumbotron>
                     <Jumbotron className='gallery-jumbotron'>
                         <Image src={rinqydink1} className='gallery' />
                     </Jumbotron>
